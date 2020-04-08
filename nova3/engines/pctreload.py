9cfda499f4395d550cf518e0b0d8c3a575c5241d
Jose Lorenzo <jose.lorenzo117@alu.ulpgc.es>
2020-04-08 23:13:13 +0100

    Add files via upload

--- nova3/engines/pctreload.py
+++ nova3/engines/pctreload.py
@@ -1,4 +1,4 @@
-#VERSION: 1.00
+#VERSION: 1.1
 #AUTHORS: Jose Lorenzo (josee.loren@gmail.com)
 
 from helpers import download_file, retrieve_url, headers
@@ -8,10 +8,30 @@ import json
 import urllib.error
 import urllib.parse
 import urllib.request
+try:
+    #python3
+    from html.parser import HTMLParser
+except ImportError:
+    #python2
+    from HTMLParser import HTMLParser
 
 class pctreload(object):
     url = 'https://pctreload.com'
     name = 'PCTReload'
+    size = ""
+    
+    class HTMLParser1(HTMLParser):
+        indicador = 0
+        def handle_starttag(self, tag, attrs):
+            if tag == 'a' and self.indicador == 1:
+                Dict = dict(attrs)
+                #print(Dict["href"])
+                pctreload.get_torrent2(self, Dict["href"])
+                self.indicador = 0
+            elif tag == "span":
+                Dict = dict(attrs)
+                if "class" in Dict and Dict["class"] == "color":
+                    self.indicador = 1
 
     def do_post(self, full_url, what):
         query_args = {'s': what, 'pg': self.pg}
@@ -22,20 +42,44 @@ class pctreload(object):
             self.pg = self.pg + 1
             return the_page
             
-    def get_torrent(self, m):
-        name = m.split("/")[4].split('.')[0]
-        link = self.url+"/descargar-torrent/"+name
+    def montar_torrent(self, link):
+        num = -1
+        while link.split("/")[num].split('.')[0] == "":
+            num = num - 1
+            name = link.split("/")[num].split('.')[0]
+        
+        link = pctreload.url + link[link.find("/"):]
         
         item = {}
         item['seeds'] = '-1'
         item['leech'] = '-1'
         item['name'] = name
-        item['size'] = self.size
+        item['size'] = pctreload.size
         item['link'] = link
-        item['engine_url'] = self.url
+        item['engine_url'] = pctreload.url
         item['desc_link'] = link
 
+        #print(item)
         prettyPrinter(item)
+        
+    def get_torrent_core(self, link):
+        html_virgen = retrieve_url(link)
+        html = html_virgen[html_virgen.find("window.location.href = \"//"):]
+        html = html[:html.find("\";")]
+        html = html[26:]
+        if html == "":
+            parser = self.HTMLParser1()
+            parser.feed(html_virgen)
+        else:
+            pctreload.montar_torrent(self,html)
+        return
+    
+    def get_torrent2(self, link):
+        pctreload.get_torrent_core(self, link)
+    
+    def get_torrent(self, guid):
+        link = self.url + "/" +  guid
+        pctreload.get_torrent_core(self, link)
     
     def search(self, what, cat='all'):
         self.pg = 1
@@ -43,6 +87,7 @@ class pctreload(object):
         while self.pg > 0:
             json_data = self.do_post(self.url+'/get/result/', what)
             torrents = json.loads(json_data)['data']['torrents']
+            #print (torrents)
             
             for k, v in torrents.items():
                 if v == None:
@@ -50,8 +95,13 @@ class pctreload(object):
                 for k2, v2 in v.items():
                     for k3, v3 in v2.items():
                         if k3 == 'torrentSize':
-                            self.size = v3
-                        if k3 == 'imagen':
+                            pctreload.size = v3
+                        if k3 == 'guid':
                             self.get_torrent(v3)
                             
+                            
             self.pg = self.pg + 1
+
+if __name__ == "__main__":
+    m = pctreload()
+    m.search('bloodshot')
